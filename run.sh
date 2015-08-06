7e68204cabc243a507e98599743a4be33066300c
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-08-06 13:35:14 +0100

    Added kb8 support

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo -e
+
+source $SECRETS_PATH
+
 echo "{
   \"consumer_key\": \"${TWITTER_CONSUMER_KEY}\",
   \"consumer_secret\": \"${TWITTER_CONSUMER_SECRET}\",
@@ -7,6 +11,12 @@ echo "{
   \"oauth_token_secret\": \"${TWITTER_TOKEN_SECRET}\"
 }"  > /root/.ebooksrc
 
+export TWITTER_CONSUMER_KEY
+export TWITTER_CONSUMER_SECRET
+export TWITTER_ACCESS_KEY
+export TWITTER_TOKEN_SECRET
+
 bundle exec ebooks archive purplebooth corpus/purplebooth.json
 bundle exec ebooks consume corpus/purplebooth.json
 bundle exec ebooks start
+
